9d5ae2b6319312a350ecc2ff197f67dfec9e71ad
unknown <roca@.hlu.corp.lundbeck.com>
2009-03-27 16:29:49 -0400

    Almost done with chapter 8. I have to todo the playtime extras

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,9 +1,30 @@
+#---
+# Excerpted from "Agile Web Development with Rails, 3rd Ed.",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails3 for more book information.
+#---
 class Cart
-   attr_reader :items 
-     def initialize
-     @items = []
+  attr_reader :items   
+  
+  def initialize
+    @items = []
   end
+  
+  
   def add_product(product)
-     @items << product
+    current_item = @items.find {|item| item.product == product}
+    if current_item
+      current_item.increment_quantity
+    else
+      @items << CartItem.new(product)
+    end
   end
+
+  def total_price
+   @items.sum { |item| item.price }
+  end
+  
 end
