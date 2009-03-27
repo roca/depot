7fbf3ee19efcb047792754f8cd320c663bd06311
unknown <roca@.hlu.corp.lundbeck.com>
2009-03-27 13:25:14 -0400

    More code from book

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '1e5c1e1fc71c86bc097bd79e7012c552'
+  protect_from_forgery  :secret => '1e5c1e1fc71c86bc097bd79e7012c552'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090320194401) do
+ActiveRecord::Schema.define(:version => 20090327155841) do
 
   create_table "products", :force => true do |t|
     t.string  "title"
@@ -17,5 +17,15 @@ ActiveRecord::Schema.define(:version => 20090320194401) do
     t.string  "image_url"
     t.decimal "price",       :precision => 8, :scale => 2, :default => 0.0
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
 end
