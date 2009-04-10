5dbaeffe23bff4f2a4a9094c91130bb5d16c63fb
unknown <roca@.hlu.corp.lundbeck.com>
2009-04-10 14:18:45 -0400

    Up to 9.2 Iteration D2: Creating an Ajax-Based Cart ( Page 130)

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,47 +1,54 @@
+#---
+# Excerpted from "Agile Web Development with Rails, 3rd Ed.",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails3 for more book information.
+#---
 class StoreController < ApplicationController
+  
   def index
     @products = Product.find_products_for_sale
-
-    @count = increment_count
-
+    @cart = find_cart
   end
-
-
-  def add_to_cart
-
-       session[:counter] = nil
-
-      product = Product.find(params[:id])
-      @cart = find_cart
-      @cart.add_product(product)
-
-    rescue ActiveRecord::RecordNotFound
-      logger.error("Attempt to access invalid product #{params[:id]}" )
-      redirect_to_index("Invalid product" )
-  end
-
-  def empty_cart
-    session[:cart] = nil
-    redirect_to_index("Your cart is currently empty" )
-  end
-
   
 
-  private
 
-  def redirect_to_index(msg)
-    flash[:notice] = msg
+  
+  
+  def add_to_cart
+    product = Product.find(params[:id])
+    @cart = find_cart
+    @cart.add_product(product)
+      redirect_to_index
+  rescue ActiveRecord::RecordNotFound
+    logger.error("Attempt to access invalid product #{params[:id]}")
+    redirect_to_index("Invalid product")
+  end
+  
+
+
+  
+  def empty_cart
+    session[:cart] = nil
+    redirect_to_index("Your cart is currently empty")
+  end
+  
+
+private
+
+  
+  def redirect_to_index(msg = nil)
+    flash[:notice] = msg if msg
     redirect_to :action => 'index'
   end
+  
+  
 
   def find_cart
     session[:cart] ||= Cart.new
   end
 
-
-
-def increment_count
-  session[:counter]  = session[:counter] ?  session[:counter] + 1 : 0
-end
 
 end
