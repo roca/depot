920f2a5bc1b2cc615f0d7ee9dfcb77aaea1e9af3
Romel Campbell <romel.campbell@tarcml28343.local>
2011-05-09 15:39:27 -0400

    Intialized new version from Fourth Edition from book

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = 'f5d08a1c94066f8692cef709a1abdd04729145cb84ba38f6568d1884f35faf8ba9a0994c6eec51017237e576a041ee0d4772d8bdf02fa9cbe001b968f71e48cf'
+Depot::Application.config.secret_token = '290e1621e12132a7b91b27b1931e69cf656e9a60f92096d702d4b88061d1baf84664ec703b1b8fd88f6eb8fb1830e759b2192fbea6966f63fd30ac3d8b96a696'
